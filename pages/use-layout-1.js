c7c1651ecbf607f2163bd2937d02dfecb9312d89
LiuZiMing1104 <lzmatdev@gmail.com>
2020-02-24 13:56:38 +0900

    creating initial index page

--- pages/use-layout-1.js
+++
@@ -1,7 +0,0 @@
-// pages/index.js
-
-import TestLayout1 from '../components/TestLayout1';
-
-const Page = () => <p>This is the first way</p>;
-
-export default TestLayout1(Page);
--- pages/use-layout-2.js
+++
@@ -1,9 +0,0 @@
-// pages/index.js
-
-import TestLayout2 from '../components/TestLayout2';
-
-const pageContent = <p>This is the second way</p>;
-
-export default function uselayout2 () {
-    return <TestLayout2 content={pageContent} />
-}
