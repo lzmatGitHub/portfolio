78ca92a40f1e0ea36a2aa8655dc90a01597651e3
LiuZiMing1104 <lzmatdev@gmail.com>
2020-03-23 23:22:09 +0900

    adding database route

---
+++ pages/api/cards.js
@@ -0,0 +1,14 @@
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./db.sqlite3');
+
+export default (req, res) => {
+    let sql = `SELECT * FROM cards`;
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        if (rows) {
+            res.status(200).json(rows);
+        }
+    });
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import HeadConfig from '../components/Component/HeadConfig';
 import IndexLayout from '../components/Layout/IndexLayout';
+import useSWR from 'swr';
+
+function fetcher(url) {
+    return fetch(url).then(r => r.json());
+}
 
 export default function Index() {
+    const { data, error } = useSWR('/api/cards', fetcher);
+    console.log(data);
     return (
         <div>
             <HeadConfig title="劉のポートフォリオサイト" description="React.jsとNext.jsで作成したポートフォリオサイト"/>
