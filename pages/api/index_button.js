c3fcbb06d5316f51dcafe853c3d9b737516450b6
LiuZiMing1104 <lzmatdev@gmail.com>
2020-04-04 14:47:34 +0900

    fixing sqlite3 issue - deploy test

--- pages/api/index_button.js
+++ pages/api/index_button.js
@@ -1,7 +1,7 @@
-const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('./db.sqlite3');
-
 export default (req, res) => {
+    const sqlite3 = require('sqlite3');
+    const db = new sqlite3.Database('db.sqlite3');
+
     let sql = `SELECT * FROM index_button`;
     try {
         db.all(sql, [], (err, rows) => {
