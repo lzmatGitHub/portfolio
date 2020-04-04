b759fa90ad988bf26a672b19423604f0266e0551
LiuZiMing1104 <lzmatdev@gmail.com>
2020-04-04 13:37:31 +0900

    adding try-catch

--- pages/api/index_button.js
+++ pages/api/index_button.js
@@ -3,12 +3,16 @@ const db = new sqlite3.Database('./db.sqlite3');
 
 export default (req, res) => {
     let sql = `SELECT * FROM index_button`;
-    db.all(sql, [], (err, rows) => {
-        if (err) {
-            throw err;
-        }
-        if (rows) {
-            res.status(200).json(rows);
-        }
-    });
+    try {
+        db.all(sql, [], (err, rows) => {
+            if (err) {
+                throw err;
+            }
+            if (rows) {
+                res.status(200).json(rows);
+            }
+        });
+    } catch(err) {
+        console.error(`Error: ${err.message}`);
+    }
 }
